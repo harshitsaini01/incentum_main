a145b4a8dbc337bb08885f5240f25bbbd98c6bff
Harshit Saini <harshitsaini0019@gmail.com>
2025-10-30 02:07:24 +0530

    major delete

--- frontend/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import formReducer from './formOneSlice';
-
-
-export const store = configureStore({
-  reducer: {
-    form: formReducer,  
-  }
-});
